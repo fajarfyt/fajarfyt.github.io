ee8c3007ac5fb98bc0fda01c9756016dda1187cb
Fajar <fajarfyt@gmail.com>
2020-09-03 21:52:50 +0700

    edit

--- js/main.js
+++ js/main.js
@@ -346,6 +346,29 @@ window.onload = function() {
   document.body.appendChild(css);
 };
 
+// CARD SLIDE
+$('#recipeCarousel').carousel({
+	interval: 5000
+})
+
+$('.carousel .carousel-item').each(function () {
+	var minPerSlide = 3;
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i = 0; i < minPerSlide; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+	}
+});
+
 
 })(jQuery);
 
